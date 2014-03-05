8062f1b1ed72e0bc2d716082858e64e4024ad91e
kubo <kubo@future-lab.asia>
2014-03-05 19:21:15 +0900

    no message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KintaiApp::Application.config.secret_key_base = '15e8e1fed7a5dd539a3c15a55c4cce93d4016cba5001a786d6f8eb0cfbc833a88eaed121354ae7beaca7fdcd3ce30f79784381efdb7f7b8b6debb0cee9712848'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KintaiApp::Application.config.session_store :cookie_store, key: '_kintai-app_session'
