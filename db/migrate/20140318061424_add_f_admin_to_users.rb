88bf4019ae2a7672a0c82cede56495fe8725720c
kubo <kubo@future-lab.asia>
2014-03-18 17:24:35 +0900

    no message

--- db/migrate/20140318061424_add_f_admin_to_users.rb
+++ db/migrate/20140318061424_add_f_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddFAdminToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :f_admin, :boolean, :default => 0
+    add_column :users, :f_admin, :boolean, :default => "0"
   end
 end
